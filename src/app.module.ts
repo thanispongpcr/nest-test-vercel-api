9ff88f1c1df84ff86293e3545bc99c283ad4f44d
thanispongpcr <naynay.pholcharoen@gmail.com>
2024-01-21 21:26:06 +0700

    test typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApiModule } from './api/api.module';
+import { Api } from './api/entities/api.entity';
 
 @Module({
-  imports: [ApiModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: 5432,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    entities: [Api],
+    synchronize: false
+  }),
+    ApiModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
